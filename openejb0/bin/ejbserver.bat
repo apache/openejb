a0c5f1d15fff74cf9ad2c6a1d1cf13949a28488e
Alan Cabrera <adc@apache.org>
2002-06-22 21:25:41 +0000

    Server should also include OPENEJB_HOME\beans in its CLASSPATH to include deployed beans.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk@443817 13f79535-47bb-0310-9956-ffa450edef68

--- openejb0/bin/ejbserver.bat
+++ openejb0/bin/ejbserver.bat
@@ -8,6 +8,7 @@ set JAVA=%JAVA_HOME%\bin\java
 set cp=
 for %%i in (lib\*.jar) do call cp.bat %%i
 for %%i in (lib\*.zip) do call cp.bat %%i
+for %%i in (beans\*.jar) do call cp.bat %%i
 for %%i in (dist\*.jar) do call cp.bat %%i
 set CLASSPATH=%JAVA_HOME%\lib\tools.jar;%CP%
 
