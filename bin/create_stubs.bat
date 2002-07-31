a18dbb595f644daf1319991ca7b52a27b29f3883
Alan Cabrera <adc@apache.org>
2002-07-31 05:11:52 +0000

    587783 The input line is too long.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb0@443876 13f79535-47bb-0310-9956-ffa450edef68

--- bin/create_stubs.bat
+++ bin/create_stubs.bat
@@ -7,12 +7,22 @@ if "%OPENEJB_HOME%"=="" set OPENEJB_HOME=%CD%
 set JAVA=%JAVA_HOME%/bin/java
 
 REM=== Setup Classpath ===
-set CP=%CLASSPATH%
-for %%i in (%OPENEJB_HOME%\lib\*.jar) do call cp.bat %%i
-for %%i in (%OPENEJB_HOME%\lib\*.zip) do call cp.bat %%i
-for %%i in (%OPENEJB_HOME%\dist\*.jar) do call cp.bat %%i
-for %%i in (%OPENEJB_HOME%\dist\*.zip) do call cp.bat %%i
-for %%i in (%OPENEJB_HOME%\test\lib\*.jar) do call cp.bat %%i
+
+set CP=
+for %%i in (%OPENEJB_HOME%\dist\openejb*.jar) do call cp.bat %%i
+for %%i in (%OPENEJB_HOME%\lib\openejb*.jar) do call cp.bat %%i
+
+set CP=%OPENEJB_HOME%\lib\castor-0.9.3.9.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\ejb-1.0.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\idb_3.26.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\jakarta-regexp-1.1.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\jca_1.0.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\jta_1.0.1.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\junit_3.5.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\log4j-1.2.1.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\xercesImpl-2.0.2.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\xmlParserAPIs-2.0.2.jar;%CP%
+
 set CP=%JAVA_HOME%\lib\tools.jar;%CP%
 
 set OPTIONS=
--- bin/launch_client.bat
+++ bin/launch_client.bat
@@ -7,20 +7,27 @@ REM      David Blevins <david.blevins@visi.com>
 if "%OPENEJB_HOME%"=="" set OPENEJB_HOME=%CD%
 
 set JAVA=%JAVA_HOME%\bin\java
-set cp=%JAVA_HOME%\lib\tools.jar
-for %%i in (%OPENEJB_HOME%\lib\*.jar) do call cp.bat %%i
-for %%i in (%OPENEJB_HOME%\lib\*.zip) do call cp.bat %%i
-set OPENEJB_CP=%JAVA_HOME%\lib\tools.jar;%OPENEJB_HOME%\lib\xerces-J_1.3.1.jar;%CP%
 
-set cp=
-for %%i in (%OPENEJB_HOME%\dist\*client*.jar) do call cp.bat %%i
-for %%i in (%OPENEJB_HOME%\dist\*test*.jar) do call cp.bat %%i
-for %%i in (%OPENEJB_HOME%\dist\openejb-*.jar) do call cp.bat %%i
-set test_cp=%OPENEJB_CP%;%CP%
+set CP=
+for %%i in (%OPENEJB_HOME%\dist\openejb*.jar) do call cp.bat %%i
+for %%i in (%OPENEJB_HOME%\lib\openejb*.jar) do call cp.bat %%i
+
+set CP=%OPENEJB_HOME%\lib\castor-0.9.3.9.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\ejb-1.0.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\idb_3.26.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\jakarta-regexp-1.1.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\jca_1.0.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\jta_1.0.1.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\junit_3.5.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\log4j-1.2.1.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\xercesImpl-2.0.2.jar;%CP%
+set CP=%OPENEJB_HOME%\lib\xmlParserAPIs-2.0.2.jar;%CP%
+
+set CP=%JAVA_HOME%\lib\tools.jar;%CP%
 
 set ORB=-DORBProfile=ejb -Djava.naming.factory.initial=org.openorb.rmi.jndi.CtxFactory -Dorg.omg.CORBA.ORBClass=org.openorb.CORBA.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.openorb.CORBA.ORBSingleton -Djavax.rmi.CORBA.StubClass=org.openorb.rmi.system.StubDelegateImpl -Djavax.rmi.CORBA.UtilClass=org.openorb.rmi.system.UtilDelegateImpl -Djavax.rmi.CORBA.PortableRemoteObjectClass=org.openorb.rmi.system.PortableRemoteObjectDelegateImpl
 set SERVER=-Dopenejb.test.server=org.openejb.test.CorbaTestServer
 set DATABASE=-Dopenejb.test.database=org.openejb.test.InstantDbTestDatabase
 set OPTIONS=%SERVER% %DATABASE% %ORB%
 
-%JAVA% %OPTIONS% -classpath %test_cp% org.openejb.test.ClientTestRunner -s src\tests-ejb\CorbaServer_config.properties org.openejb.test.ClientTestSuite
+%JAVA% %OPTIONS% -classpath %CP% org.openejb.test.ClientTestRunner -s src\tests-ejb\CorbaServer_config.properties org.openejb.test.ClientTestSuite
