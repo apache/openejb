cf9c8f957a9338f9296ad731d1481fc6ef3ac5b2
David Blevins <dblevins@apache.org>
2002-08-20 02:34:45 +0000

    Added HomeHandle support back into the interface.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk@443905 13f79535-47bb-0310-9956-ffa450edef68

--- openejb0/src/main/org/openejb/spi/ApplicationServer.java
+++ openejb0/src/main/org/openejb/spi/ApplicationServer.java
@@ -3,7 +3,7 @@ package org.openejb.spi;
 import org.openejb.ProxyInfo;
 import javax.ejb.EJBMetaData;
 import javax.ejb.Handle;
-//import javax.ejb.HomeHandle;
+import javax.ejb.HomeHandle;
 import javax.ejb.EJBObject;
 import javax.ejb.EJBHome;
 
@@ -65,6 +65,8 @@ public interface ApplicationServer {
     public EJBMetaData getEJBMetaData(ProxyInfo proxyInfo);
     
     public Handle getHandle(ProxyInfo proxyInfo);
+       
+    public HomeHandle getHomeHandle(ProxyInfo proxyInfo);
        
     public EJBObject getEJBObject(ProxyInfo proxyInfo);
     
