7ea0179cad14a7427918bb17cc865396910475b7
Daniel Stefan Haischt <dsh@apache.org>
2002-09-09 17:59:58 +0000

    modified location of cp.bat which did not point to bin\cp.bat.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk@443960 13f79535-47bb-0310-9956-ffa450edef68

--- openejb0/build.bat
+++ openejb0/build.bat
@@ -8,8 +8,8 @@ REM      Gérald Quintana
 
 set JAVA=%JAVA_HOME%\bin\java
 set cp=
-for %%i in (lib\*.jar) do call cp.bat %%i
-for %%i in (lib\*.zip) do call cp.bat %%i
+for %%i in (lib\*.jar) do call bin\cp.bat %%i
+for %%i in (lib\*.zip) do call bin\cp.bat %%i
 set CP=%JAVA_HOME%\lib\tools.jar;%CP%
 set CP=lib\xerces-J_1.3.1.jar;%CP%
 
