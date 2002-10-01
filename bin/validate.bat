97ba41b70fea00286a4462f6ad1c1009f066aeb4
David Blevins <dblevins@apache.org>
2002-10-01 18:23:23 +0000

    Added a bat version of the validate script.  The tool isn't fully ready, but is still pretty useful, so a bat script is nice.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb0@444000 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bin/validate.bat
@@ -0,0 +1,23 @@
+@echo off
+REM $Id$
+
+REM   Contributions by:
+REM      David Blevins <david.blevins@visi.com>
+
+if "%OPENEJB_HOME%"=="" set OPENEJB_HOME=%CD%
+
+set JAVA=%JAVA_HOME%\bin\java
+
+set CP=
+for %%i in (%OPENEJB_HOME%\dist\openejb*.jar) do call cp.bat %%i
+set CP=%JAVA_HOME%\lib\tools.jar;%CP%
+
+set OPTIONS=-Dopenejb.home=%OPENEJB_HOME%
+
+echo --------------SUPPORT INFO-------------
+echo %OS%
+echo Using JAVA_HOME:     %JAVA_HOME%
+echo Using OPENEJB_HOME:  %OPENEJB_HOME%
+echo .
+
+%JAVA% %OPTIONS% -classpath %CP% -Dopenejb.home=$OPENEJB_HOME org.openejb.alt.config.EjbValidator  %1 %2 %3 %4 %5 %6 %7 %8 %9
