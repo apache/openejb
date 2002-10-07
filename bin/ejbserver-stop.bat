fce2a48ac8d3c7352e98e7a0d8cca13119487706
Alan Cabrera <adc@apache.org>
2002-10-07 02:42:09 +0000

    Should always use CP for the classpath
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb0@444021 13f79535-47bb-0310-9956-ffa450edef68

--- bin/ejbserver-stop.bat
+++ bin/ejbserver-stop.bat
@@ -11,7 +11,6 @@ set JAVA=%JAVA_HOME%\bin\java
 set CP=
 for %%i in (%OPENEJB_HOME%\dist\openejb*.jar) do call cp.bat %%i
 set CP=%JAVA_HOME%\lib\tools.jar;%CP%
-set CLASSPATH=%CLASSPATH%;%CP%
 
 set OPTIONS=-Dopenejb.home=%OPENEJB_HOME%
 
@@ -20,6 +19,6 @@ echo %OS%
 echo Using JAVA_HOME:     %JAVA_HOME%
 echo Using OPENEJB_HOME:  %OPENEJB_HOME%
 echo .
-%JAVA% %OPTIONS% -classpath %CLASSPATH% org.openejb.server.Stop  %1 %2 %3 %4 %5 %6
+%JAVA% %OPTIONS% -classpath %CP% org.openejb.server.Stop  %1 %2 %3 %4 %5 %6
 
 
