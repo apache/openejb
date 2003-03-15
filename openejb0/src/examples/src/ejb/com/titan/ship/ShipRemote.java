cea517963d1fac3002dc53718e4336bd68a277a6
Alan Cabrera <adc@apache.org>
2003-03-15 17:35:29 +0000

    635532 Add basic EJB examples
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk@444227 13f79535-47bb-0310-9956-ffa450edef68

---
+++ openejb0/src/examples/src/ejb/com/titan/ship/ShipRemote.java
@@ -0,0 +1,13 @@
+package com.titan.ship;
+
+import javax.ejb.EJBObject;
+import java.rmi.RemoteException;
+
+public interface ShipRemote extends javax.ejb.EJBObject {
+    public String getName() throws RemoteException;
+    public void setName(String name) throws RemoteException;
+    public void setCapacity(int cap) throws RemoteException;
+    public int getCapacity() throws RemoteException;
+    public double getTonnage() throws RemoteException;
+    public void setTonnage(double tons) throws RemoteException;
+}
