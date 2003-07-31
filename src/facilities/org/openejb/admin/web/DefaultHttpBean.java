8fa628a33068af1ca261138d9b314a31713349dd
sourceforge-tim273 <dev-null@apache.org>
2003-07-31 20:27:20 +0000

    Fixed stylesheet bug in Mozilla
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb0@444411 13f79535-47bb-0310-9956-ffa450edef68

--- src/facilities/org/openejb/admin/web/DefaultHttpBean.java
+++ src/facilities/org/openejb/admin/web/DefaultHttpBean.java
@@ -56,139 +56,146 @@ import javax.ejb.SessionContext;
  * @author <a href="mailto:david.blevins@visi.com">David Blevins</a>
  */
 public class DefaultHttpBean implements HttpBean {
-    /** The base url for this page */    
-    private static final URL BASE_URL = getBaseUrl();
-    /** the ejb session context */    
-    private SessionContext context;
+	/** The base url for this page */
+	private static final URL BASE_URL = getBaseUrl();
+	/** the ejb session context */
+	private SessionContext context;
 
-    /** gets the base URL for this page
-     * @return The base URL
-     */    
-    private static URL getBaseUrl(){
-        URL url = null;
-        try{
-            url = new URL("resource:/openejb/webadmin");
-        } catch (Exception e){
-            // TODO: 1: We should never get an exception here
-            e.printStackTrace();
-        }
-        return url;
-    }
+	/** gets the base URL for this page
+	 * @return The base URL
+	 */
+	private static URL getBaseUrl() {
+		URL url = null;
+		try {
+			url = new URL("resource:/openejb/webadmin");
+		} catch (Exception e) {
+			// TODO: 1: We should never get an exception here
+			e.printStackTrace();
+		}
+		return url;
+	}
 
-    /** Creates a new instance */
-    public void ejbCreate() {
-    }
+	/** Creates a new instance */
+	public void ejbCreate() {}
 
-    /** the main processing part of the this bean
-     * @param request the http request object
-     * @param response the http response object
-     * @throws IOException if an exception is thrown
-     */    
-    public void onMessage(HttpRequest request, HttpResponse response) throws java.io.IOException{
-        // Internationalize this
-        try {
-            String file = request.getURI().getFile();
-            String ext = ( file.indexOf('.') == -1 )? null:file.substring(file.indexOf('.'));
-            
-            if ( ext != null ) {
-                if ( ext.equalsIgnoreCase(".gif") ) {
-                    response.setContentType("image/gif");
-                } else if ( ext.equalsIgnoreCase(".jpeg") || ext.equalsIgnoreCase(".jpg") ) {
-                    response.setContentType("image/jpeg");
-                } else if ( ext.equalsIgnoreCase(".png")) {
-                    response.setContentType("image/png");
-                }
-            }
+	/** the main processing part of the this bean
+	 * @param request the http request object
+	 * @param response the http response object
+	 * @throws IOException if an exception is thrown
+	 */
+	public void onMessage(HttpRequest request, HttpResponse response) throws java.io.IOException {
+		// Internationalize this
+		try {
+			String file = request.getURI().getFile();
+			String ext = (file.indexOf('.') == -1) ? null : file.substring(file.indexOf('.'));
 
-            InputStream in = new URL(BASE_URL+request.getURI().getFile() ).openConnection().getInputStream();
-            OutputStream out = response.getOutputStream();
+			if (ext != null) {
+				//resolve the content type
+				if (ext.equalsIgnoreCase(".gif")) {
+					response.setContentType("image/gif");
+				} else if (ext.equalsIgnoreCase(".jpeg") || ext.equalsIgnoreCase(".jpg")) {
+					response.setContentType("image/jpeg");
+				} else if (ext.equalsIgnoreCase(".png")) {
+					response.setContentType("image/png");
+				} else if (ext.equalsIgnoreCase(".css")) {
+					response.setContentType("text/css");
+				} else if (ext.equalsIgnoreCase(".js")) {
+					response.setContentType("text/javascript");
+				} else if (ext.equalsIgnoreCase(".txt")) {
+					response.setContentType("text/plain");
+				}
+			}
 
-            int b = in.read();
-            while (b != -1) {
-                out.write( b );
-                b = in.read();
-            }
-        } catch (java.io.FileNotFoundException e) {
-            do404(request, response);
-        } catch (java.io.IOException e) {
-            do500(request, response, e.getMessage());
-        }
-    }
+			InputStream in = new URL(BASE_URL + request.getURI().getFile()).openConnection().getInputStream();
+			OutputStream out = response.getOutputStream();
 
-    /** Creates a "Page not found" error screen
-     * @param request the HTTP request object
-     * @param response the HTTP response object
-     */    
-    public void do404(HttpRequest request, HttpResponse response){
-        response.reset(404, "Object not found.");
-        java.io.PrintWriter body = response.getPrintWriter();
+			int b = in.read();
+			while (b != -1) {
+				out.write(b);
+				b = in.read();
+			}
+		} catch (java.io.FileNotFoundException e) {
+			do404(request, response);
+		} catch (java.io.IOException e) {
+			do500(request, response, e.getMessage());
+		}
+	}
 
-        body.println("<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">");
-        body.println("<HTML><HEAD>");
-        body.println("<TITLE>404 Not Found</TITLE>");
-        body.println("</HEAD><BODY>");
-        body.println("<H1>Not Found</H1>");
-        body.println("The requested URL <font color=\"red\">"+request.getURI().getFile()+"</font> was not found on this server.<P>");
-        body.println("<HR>");
-        body.println("<ADDRESS>"+response.getServerName()+"</ADDRESS>");
-        body.println("</BODY></HTML>");
-    }
+	/** Creates a "Page not found" error screen
+	 * @param request the HTTP request object
+	 * @param response the HTTP response object
+	 */
+	public void do404(HttpRequest request, HttpResponse response) {
+		response.reset(404, "Object not found.");
+		java.io.PrintWriter body = response.getPrintWriter();
 
-    /** Creates and "Internal Server Error" page
-     * @param request the HTTP request object
-     * @param response the HTTP response object
-     * @param message the message to be sent back to the browser
-     */    
-    public void do500(HttpRequest request, HttpResponse response, String message){
-        response.reset(500, "Internal Server Error.");
-        java.io.PrintWriter body = response.getPrintWriter();
-        body.println("<html>");
-        body.println("<body>");
-        body.println("<h3>Internal Server Error</h3>");
-        body.println("<br><br>");
+		body.println("<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">");
+		body.println("<HTML><HEAD>");
+		body.println("<TITLE>404 Not Found</TITLE>");
+		body.println("</HEAD><BODY>");
+		body.println("<H1>Not Found</H1>");
+		body.println(
+			"The requested URL <font color=\"red\">"
+				+ request.getURI().getFile()
+				+ "</font> was not found on this server.<P>");
+		body.println("<HR>");
+		body.println("<ADDRESS>" + response.getServerName() + "</ADDRESS>");
+		body.println("</BODY></HTML>");
+	}
 
-        if (message != null) {
-            StringTokenizer msg = new StringTokenizer(message, "\n\r");
-            while (msg.hasMoreTokens()) {
-                body.print( msg.nextToken() );
-                body.println("<br>");
-            }
-        }
-        
-        body.println("</body>");
-        body.println("</html>");
-    }
+	/** Creates and "Internal Server Error" page
+	 * @param request the HTTP request object
+	 * @param response the HTTP response object
+	 * @param message the message to be sent back to the browser
+	 */
+	public void do500(HttpRequest request, HttpResponse response, String message) {
+		response.reset(500, "Internal Server Error.");
+		java.io.PrintWriter body = response.getPrintWriter();
+		body.println("<html>");
+		body.println("<body>");
+		body.println("<h3>Internal Server Error</h3>");
+		body.println("<br><br>");
 
-    /** called on a stateful sessionbean after the bean is
-     * deserialized from storage and put back into use.      
-     * @throws EJBException if an exeption is thrown
-     * @throws RemoteException if an exception is thrown
-     */    
-    public void ejbActivate() throws javax.ejb.EJBException, java.rmi.RemoteException {
-    }
+		if (message != null) {
+			StringTokenizer msg = new StringTokenizer(message, "\n\r");
+			while (msg.hasMoreTokens()) {
+				body.print(msg.nextToken());
+				body.println("<br>");
+			}
+		}
 
-    /** called on a stateful sessionbean before the bean is 
-     * removed from memory and serialized to a temporary store.  
-     * This method is never called on a stateless sessionbean
-     * @throws EJBException if an exception is thrown
-     * @throws RemoteException if an exception is thrown
-     */   
-    public void ejbPassivate() throws javax.ejb.EJBException, java.rmi.RemoteException {
-    }
+		body.println("</body>");
+		body.println("</html>");
+	}
 
-    /** called by the ejb container when this bean is about to be garbage collected
-     * @throws EJBException if an exception is thrown
-     * @throws RemoteException if an exception is thrown
-     */    
-    public void ejbRemove() throws javax.ejb.EJBException, java.rmi.RemoteException {
-    }
+	/** called on a stateful sessionbean after the bean is
+	 * deserialized from storage and put back into use.      
+	 * @throws EJBException if an exeption is thrown
+	 * @throws RemoteException if an exception is thrown
+	 */
+	public void ejbActivate() throws javax.ejb.EJBException, java.rmi.RemoteException {}
 
-    /** sets the session context for this bean
-     * @param sessionContext the session context to be set
-     * @throws EJBException if an exception is thrown
-     * @throws RemoteException if an exception is thrown
-     */    
-    public void setSessionContext(javax.ejb.SessionContext sessionContext) throws javax.ejb.EJBException, java.rmi.RemoteException {
-        this.context = sessionContext;
-    }
+	/** called on a stateful sessionbean before the bean is 
+	 * removed from memory and serialized to a temporary store.  
+	 * This method is never called on a stateless sessionbean
+	 * @throws EJBException if an exception is thrown
+	 * @throws RemoteException if an exception is thrown
+	 */
+	public void ejbPassivate() throws javax.ejb.EJBException, java.rmi.RemoteException {}
+
+	/** called by the ejb container when this bean is about to be garbage collected
+	 * @throws EJBException if an exception is thrown
+	 * @throws RemoteException if an exception is thrown
+	 */
+	public void ejbRemove() throws javax.ejb.EJBException, java.rmi.RemoteException {}
+
+	/** sets the session context for this bean
+	 * @param sessionContext the session context to be set
+	 * @throws EJBException if an exception is thrown
+	 * @throws RemoteException if an exception is thrown
+	 */
+	public void setSessionContext(javax.ejb.SessionContext sessionContext)
+		throws javax.ejb.EJBException, java.rmi.RemoteException {
+		this.context = sessionContext;
+	}
 }
