73d1870921bcec51774aa682b445514d5a0c53d9
David Jencks <djencks@apache.org>
2004-01-01 08:35:01 +0000

    Fix npe found by Jacek
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/branches/nova@444558 13f79535-47bb-0310-9956-ffa450edef68

--- openejb0/nova/src/java/org/openejb/nova/deployment/TransactionPolicyHelper.java
+++ openejb0/nova/src/java/org/openejb/nova/deployment/TransactionPolicyHelper.java
@@ -57,10 +57,10 @@ import java.util.TreeSet;
 
 import org.apache.geronimo.deployment.model.ejb.ContainerTransaction;
 import org.apache.geronimo.deployment.model.ejb.Method;
-import org.openejb.nova.transaction.ContainerPolicy;
-import org.openejb.nova.transaction.BeanPolicy;
-import org.openejb.nova.transaction.TxnPolicy;
 import org.openejb.nova.dispatch.MethodSignature;
+import org.openejb.nova.transaction.BeanPolicy;
+import org.openejb.nova.transaction.ContainerPolicy;
+import org.openejb.nova.transaction.TxnPolicy;
 
 /**
  *
@@ -102,11 +102,12 @@ public class TransactionPolicyHelper {
         return new TransactionPolicySourceImpl((SortedSet) ejbNameToTransactionAttributesMap.get(ejbName));
     }
 
-    private class TransactionPolicySourceImpl implements TransactionPolicySource {
+    private static class TransactionPolicySourceImpl implements TransactionPolicySource {
+        private final static SortedSet EMPTY_SORTED_SET = new TreeSet();
         private final SortedSet transactionPolicies;
 
         public TransactionPolicySourceImpl(SortedSet transactionPolicies) {
-            this.transactionPolicies = transactionPolicies;
+            this.transactionPolicies = transactionPolicies == null? EMPTY_SORTED_SET: transactionPolicies;
         }
 
         public TxnPolicy getTransactionPolicy(String methodIntf, MethodSignature signature) {
