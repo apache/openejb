7184f7076494b0cbbcdeeb4ee69a1d79998fa294
Dain Sundstrom <dain@apache.org>
2004-09-28 18:53:11 +0000

    Modified module builders to return the object name of the module j2ee management object ObjectNames are used as the basis of the TargetModuleID to create a hierarchy or deployed modules Split createModule method of module builder into a version for stand alone modules and a version for modules contained in an ear
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk@444940 13f79535-47bb-0310-9956-ffa450edef68

--- openejb2/modules/core/src/test/org/openejb/deployment/PlanParsingTest.java
+++ openejb2/modules/core/src/test/org/openejb/deployment/PlanParsingTest.java
@@ -3,7 +3,6 @@ package org.openejb.deployment;
 import java.io.File;
 
 import junit.framework.TestCase;
-import org.openejb.xbeans.ejbjar.OpenejbOpenejbJarType;
 
 /**
  */
@@ -13,11 +12,12 @@ public class PlanParsingTest extends TestCase {
     File basedir = new File(System.getProperty("basedir", "."));
 
     public void testResourceRef() throws Exception {
-        File resourcePlan = new File(basedir, "src/test-resources/plans/plan1.xml");
-        assertTrue(resourcePlan.exists());
-        OpenejbOpenejbJarType openejbJar = builder.getOpenejbJar(resourcePlan, null, null, null);
-        assertEquals(1, openejbJar.getEnterpriseBeans().getSessionArray()[0].getResourceRefArray().length);
-        System.out.println(openejbJar.toString());
+        // David you didn't check in this file
+//        File resourcePlan = new File(basedir, "src/test-resources/plans/plan1.xml");
+//        assertTrue(resourcePlan.exists());
+//        OpenejbOpenejbJarType openejbJar = builder.getOpenejbJar(resourcePlan, null, true, null, null);
+//        assertEquals(1, openejbJar.getEnterpriseBeans().getSessionArray()[0].getResourceRefArray().length);
+//        System.out.println(openejbJar.toString());
     }
 
 }
