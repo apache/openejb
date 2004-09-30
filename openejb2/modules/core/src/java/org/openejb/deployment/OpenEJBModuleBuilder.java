5c70b681d98e8aeb3d7ce8997524c18799d49d55
Dain Sundstrom <dain@apache.org>
2004-09-30 18:58:19 +0000

    Updated method signature to reflect changed in geronimo ModuleBuilder interface
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk@444948 13f79535-47bb-0310-9956-ffa450edef68

--- openejb2/modules/core/src/java/org/openejb/deployment/OpenEJBModuleBuilder.java
+++ openejb2/modules/core/src/java/org/openejb/deployment/OpenEJBModuleBuilder.java
@@ -127,7 +127,7 @@ public class OpenEJBModuleBuilder implements ModuleBuilder, EJBReferenceBuilder 
         return createModule(plan, moduleFile, "war", null, true);
     }
 
-    public Module createModule(Object plan, JarFile moduleFile, String targetPath, URL specDDUrl) throws DeploymentException {
+    public Module createModule(Object plan, JarFile moduleFile, String targetPath, URL specDDUrl, URI earConfigId) throws DeploymentException {
         return createModule(plan, moduleFile, targetPath, specDDUrl, false);
     }
 
