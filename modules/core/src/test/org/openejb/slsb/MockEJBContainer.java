9c892130dd0956546171d17393c1c8e9565d532e
David Blevins <dblevins@apache.org>
2004-12-20 06:43:36 +0000

    Mock objects for testing service endpoints
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb2@445101 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/core/src/test/org/openejb/slsb/MockEJBContainer.java
@@ -0,0 +1,138 @@
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "OpenEJB" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of The OpenEJB Group.  For written permission,
+ *    please contact openejb@openejb.org.
+ *
+ * 4. Products derived from this Software may not be called "OpenEJB"
+ *    nor may "OpenEJB" appear in their names without prior written
+ *    permission of The OpenEJB Group. OpenEJB is a registered
+ *    trademark of The OpenEJB Group.
+ *
+ * 5. Due credit should be given to the OpenEJB Project
+ *    (http://openejb.org/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE OPENEJB GROUP AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE OPENEJB GROUP OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 2001 (C) The OpenEJB Group. All Rights Reserved.
+ *
+ * $Id$
+ */
+package org.openejb.slsb;
+
+import java.lang.reflect.Method;
+import javax.ejb.EJBHome;
+import javax.ejb.EJBLocalHome;
+import javax.ejb.EJBLocalObject;
+import javax.ejb.EJBObject;
+import javax.security.auth.Subject;
+
+import org.apache.geronimo.core.service.Invocation;
+import org.apache.geronimo.core.service.InvocationResult;
+import org.openejb.EJBComponentType;
+import org.openejb.EJBContainer;
+import org.openejb.dispatch.InterfaceMethodSignature;
+import org.openejb.proxy.EJBProxyFactory;
+import org.openejb.proxy.ProxyInfo;
+
+
+public class MockEJBContainer implements EJBContainer {
+    private final ProxyInfo proxyInfo;
+    private final Class ejbClass;
+
+    public MockEJBContainer() {
+        this.proxyInfo = new ProxyInfo(EJBComponentType.STATELESS,"foo",MockHome.class,MockRemote.class,MockLocalHome.class,MockLocal.class,MockServiceEndpoint.class, null);
+        ejbClass = MockEJB.class;
+    }
+
+    public Object getContainerID() {
+        return null;
+    }
+
+    public String getEJBName() {
+        return null;
+    }
+
+    public EJBHome getEJBHome() {
+        return null;
+    }
+
+    public EJBObject getEJBObject(Object primaryKey) {
+        return null;
+    }
+
+    public EJBLocalHome getEJBLocalHome() {
+        return null;
+    }
+
+    public EJBLocalObject getEJBLocalObject(Object primaryKey) {
+        return null;
+    }
+
+    public Object invoke(Method callMethod, Object[] args, Object primKey) throws Throwable {
+        Method ejbMethod = ejbClass.getMethod(callMethod.getName(), callMethod.getParameterTypes());
+
+        Object ejb = ejbClass.newInstance();
+        return ejbMethod.invoke(ejb, args);
+    }
+
+    public String[] getJndiNames() {
+        return new String[0];
+    }
+
+    public String[] getLocalJndiNames() {
+        return new String[0];
+    }
+
+    public EJBProxyFactory getProxyFactory() {
+        return null;
+    }
+
+    public ClassLoader getClassLoader() {
+        return MockEJBContainer.class.getClassLoader();
+    }
+
+    public EJBContainer getUnmanagedReference() {
+        return null;
+    }
+
+    public InterfaceMethodSignature[] getSignatures() {
+        return new InterfaceMethodSignature[0];
+    }
+
+    public ProxyInfo getProxyInfo() {
+        return proxyInfo;
+    }
+
+    public Subject getDefaultSubject() {
+        return null;
+    }
+
+    public InvocationResult invoke(Invocation invocation) throws Throwable {
+        return null;
+    }
+}
