cdd198928615ce0a2e6a82a5f9ade729e69ca75d
David Jencks <djencks@apache.org>
2005-01-26 15:13:21 +0000

    sort, expand, cleanup imports
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb2@445146 13f79535-47bb-0310-9956-ffa450edef68

--- modules/openejb-builder/src/java/org/openejb/deployment/CMPContainerBuilder.java
+++ modules/openejb-builder/src/java/org/openejb/deployment/CMPContainerBuilder.java
@@ -54,7 +54,6 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.ejb.TimedObject;
 import javax.ejb.Timer;
 import javax.management.ObjectName;
