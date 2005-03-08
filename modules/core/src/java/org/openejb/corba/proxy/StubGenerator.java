75f1aaf41274fb8edb1e820f3dc2c058a74ad5ae
Alan Cabrera <adc@apache.org>
2005-03-08 04:24:06 +0000

    Updated copyright.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb2@445260 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/java/org/openejb/corba/proxy/StubGenerator.java
+++ modules/core/src/java/org/openejb/corba/proxy/StubGenerator.java
@@ -1,11 +1,54 @@
-/*
-* Copyright (C) The Community OpenORB Project. All rights reserved.
-*
-* This software is published under the terms of The OpenORB Community Software
-* License version 1.0, a copy of which has been included with this distribution
-* in the LICENSE.txt file.
-*/
-
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "OpenEJB" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of The OpenEJB Group.  For written permission,
+ *    please contact openejb-group@openejb.sf.net.
+ *
+ * 4. Products derived from this Software may not be called "OpenEJB"
+ *    nor may "OpenEJB" appear in their names without prior written
+ *    permission of The OpenEJB Group. OpenEJB is a registered
+ *    trademark of The OpenEJB Group.
+ *
+ * 5. Due credit should be given to the OpenEJB Project
+ *    (http://openejb.sf.net/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE OPENEJB GROUP AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE OPENEJB GROUP OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 2005 (C) The OpenEJB Group. All Rights Reserved.
+ *
+ * $Id$
+ */
+/**
+ * Copyright (C) The Community OpenORB Project. All rights reserved.
+ *
+ * This software is published under the terms of The OpenORB Community Software
+ * License version 1.0, a copy of which has been included with this distribution
+ * in the LICENSE.txt file.
+ */
 package org.openejb.corba.proxy;
 
 import java.io.File;
@@ -29,14 +72,11 @@ import org.openorb.compiler.rmi.generator.Javatoidl;
 
 
 /**
- * This class takes an IDL graph and generates its IDL description.
- *
  * @author Jerome Daniel
+ * @version $Revision$ $Date$
  */
 public class StubGenerator extends Javatoidl {
-    /**
-     * Constructor
-     */
+
     public StubGenerator(RmiCompilerProperties rcp, CompilerHost ch) {
         super(rcp, ch);
     }
