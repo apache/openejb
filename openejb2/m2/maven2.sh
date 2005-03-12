3a687d2b2642ed799a72e636f892acbb042113f8
Brett Leslie Porter <brett@apache.org>
2005-03-12 05:50:43 +0000

    new build files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk@445277 13f79535-47bb-0310-9956-ffa450edef68

--- openejb2/m2/maven2.sh
+++ openejb2/m2/maven2.sh
@@ -1,42 +1,36 @@
 #!/bin/sh
 
 src=`pwd`/..
-dir=`pwd`/maven2
+dir=`pwd`/openejb
 poms=`pwd`
+modules_dir=$dir/modules
 
+echo Removing old version
 rm -rf $dir > /dev/null 2>&1
 
+echo Setting up base
 mkdir -p $dir > /dev/null 2>&1
 
 cp $poms/ejb-group.pom $dir/pom.xml
 
-cp -r $src/modules/core $dir
+echo Setting up modules
+mkdir $dir/modules
 
-mv $dir/core/src/test-ejb-jar $dir
+echo ... core
+# TODO: should be ./openejb-core not ./modules/core
+cp -r $src/modules/core $modules_dir
+cp $poms/ejb-core.pom $modules_dir/core/pom.xml
 
-# ---------------------------------------------------
-# 
-# ---------------------------------------------------
-
-(
-  cd $dir
-  m2 pom:install
-)
-
-# ---------------------------------------------------
-# 
-# ---------------------------------------------------
-
-cp $poms/ejb-test-jar.pom $dir/test-ejb-jar/pom.xml
+mv $dir/modules/core/src/test-ejb-jar $modules_dir
+cp $poms/ejb-test-jar.pom $modules_dir/test-ejb-jar/pom.xml
 
 ( 
-  cd $dir/test-ejb-jar  
+  cd $modules_dir/test-ejb-jar
   mkdir -p src/main/java
   mv org src/main/java
   mkdir -p src/main/resources
   mv META-INF src/main/resources
-  m2 pom:install
-)  
+)
 
 # ---------------------------------------------------
 
