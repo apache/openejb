2ff160411666e7cdb5f7e9e40ba950f8d217a327
Brett Leslie Porter <brett@apache.org>
2005-03-18 06:16:50 +0000

    reorg
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk@445323 13f79535-47bb-0310-9956-ffa450edef68

--- openejb2/m2/maven2.sh
+++ openejb2/m2/maven2.sh
@@ -6,29 +6,35 @@ poms=`pwd`
 modules_dir=$dir/modules
 
 echo "Backing up IntelliJ files"
-rm -rf intellij
 mkdir -p intellij
 find openejb -name '*.iml' -exec cp --parents {} intellij \;
 cp openejb/*.iws intellij/openejb
 cp openejb/*.ipr intellij/openejb
 
 echo "Removing old version"
-rm -rf $dir > /dev/null 2>&1
+rm -rf $dir
 
 echo "Setting up base"
-mkdir -p $dir > /dev/null 2>&1
-
-cp -r intellij/openejb/* openejb
+mkdir -p $dir
 
 cp $poms/ejb-group.pom $dir/pom.xml
 
 echo "Setting up modules"
-mkdir $dir/modules
+mkdir -p $modules_dir
 
 echo "... core"
 # TODO: should be ./openejb-core not ./modules/core
 cp -r $src/modules/core $modules_dir
 cp $poms/ejb-core.pom $modules_dir/core/pom.xml
+(
+  cd $modules_dir/core
+  mkdir -p src/main
+  mkdir -p src/test/java
+  mv src/conf src/main/resources
+  mv src/test/org src/test/java
+  mv src/test-resources src/test/resources
+  mv src/java src/main
+)
 
 echo "... test-ejb-jar"
 mv $dir/modules/core/src/test-ejb-jar $modules_dir
@@ -46,6 +52,14 @@ echo "... builder"
 cp -r $src/modules/openejb-builder $modules_dir
 cp $poms/ejb-builder.pom $modules_dir/openejb-builder/pom.xml
 rm -rf $modules_dir/openejb-builder/src/test-ejb-jar
+(
+  cd $modules_dir/openejb-builder
+  mkdir -p src/main
+  mkdir -p src/test/java
+  mv src/java src/main
+  mv src/test/org src/test/java
+  mv src/test-resources src/test/resources
+)
 
 echo "... test-ear"
 mv $dir/modules/openejb-builder/src/test-ear $modules_dir
@@ -65,4 +79,7 @@ cp $poms/ejb-assembly.pom $modules_dir/assembly/pom.xml
 
 echo "Removing CVS directories"
 find $dir -name 'CVS*' -exec rm -rf {} \; > /dev/null 2>&1
+
+echo "Restoring IntelliJ Files"
+cp -r intellij/openejb/* openejb
 
