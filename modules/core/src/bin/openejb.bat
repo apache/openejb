188e60c03dac51771df0f7af5c9ef890a9810adb
Jeremy Whitlock <jwhitlock@apache.org>
2006-02-23 00:27:34 +0000

    Added code back in to echo OPENEJB_HOME and also modified the openejb.bat to dynamically set OPENEJB_HOME used to locate the exectuable openejb jar.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb1@446037 13f79535-47bb-0310-9956-ffa450edef68

--- modules/core/src/bin/openejb.bat
+++ modules/core/src/bin/openejb.bat
@@ -17,14 +17,9 @@ REM================================================
 
 SETLOCAL
 
-rem find OPENEJB_HOME if it does not exist due to either an invalid value passed
-rem by the user or the %0 problem on Windows 9x
-if exist "%OPENEJB_HOME%\lib\openejb-core-*.jar" goto openejbHomeSet
+SET COMMAND=%0
+SET OPENEJB_HOME=%COMMAND:~0,-15%
 
-:noOpenEJBHome
-echo OPENEJB_HOME is set incorrectly or OpenEJB could not be located. Please set OPENEJB_HOME.
-goto EOF
-
-:openejbHomeSet
+echo OPENEJB_HOME=%OPENEJB_HOME%
 
 java %JAVA_OPTS% -jar %OPENEJB_HOME%/lib/openejb-core-@REPLACED-BY-MAVEN-XML@.jar %*
