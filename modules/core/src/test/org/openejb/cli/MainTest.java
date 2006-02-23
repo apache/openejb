e1fc0b95a5d11047b345f050cdd183ca6ae666f4
David Blevins <dblevins@apache.org>
2006-02-23 03:32:23 +0000

    Tomcat 5.5 integration works
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk/openejb1@446038 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/core/src/test/org/openejb/cli/MainTest.java
@@ -0,0 +1,14 @@
+package org.openejb.cli;
+/**
+ * @version $Revision$ $Date$
+ */
+
+import junit.framework.*;
+import org.openejb.cli.Main;
+
+public class MainTest extends TestCase {
+
+    public void testMain() throws Exception {
+        
+    }
+}
