180d682246e7d7835f2481ee4861b2a301eb882f
Jeremy Whitlock <jwhitlock@apache.org>
2007-02-14 06:12:31 +0000

    Initial contribution for the WTP Eclipse Server Type for OpenEJB.  Still needs classpath portion, start/stop portion, launcher, publisher and other bits.  For those referencing the slides from EclipseCon, I'm on slide 19 of 08-AddingNewServerAdapters.ppt.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openejb/trunk@507407 13f79535-47bb-0310-9956-ffa450edef68

---
+++ openejb-eclipse/plugins/org.apache.openejb.eclipse.wtp.server/src/org/apache/openejb/eclipse/wtp/OpenEJBPublisher.java
@@ -0,0 +1,23 @@
+package org.apache.openejb.eclipse.wtp;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jst.server.generic.core.internal.GenericPublisher;
+import org.eclipse.wst.server.core.IModuleArtifact;
+
+public class OpenEJBPublisher extends GenericPublisher {
+	
+	/**
+	 * Deploys the EJB project
+	 */
+	public IStatus[] publish(IModuleArtifact[] resource, IProgressMonitor monitor) {
+		return null;
+	}
+	
+	/**
+	 * Undeploys the EJB project
+	 */
+	public IStatus[] unpublish(IProgressMonitor monitor) {
+		return null;
+	}
+}
