8ec3377c90403f2b515741cd8568fed56b38c1de
Jonathan Gallimore <jgallimore@apache.org>
2009-02-18 16:54:34 +0000

    OEP-24 added ASF headers and changed indenting a bit
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk/sandbox/openejb-eclipse-plugin@745570 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/echo.stateless.bean/src/echo/stateless/bean/EchoStatelessPlugin.java
+++ plugins/echo.stateless.bean/src/echo/stateless/bean/EchoStatelessPlugin.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package echo.stateless.bean;
 
 import org.apache.openejb.eclipse.OpenEjbApplication;
@@ -10,53 +26,52 @@ import org.osgi.framework.BundleContext;
  */
 public class EchoStatelessPlugin extends AbstractUIPlugin {
 
-    // The plug-in ID
-    public static final String PLUGIN_ID = "echo.stateless.bean";
+	// The plug-in ID
+	public static final String PLUGIN_ID = "echo.stateless.bean";
 
-    // The shared instance
-    private static EchoStatelessPlugin plugin;
+	// The shared instance
+	private static EchoStatelessPlugin plugin;
 
-    /**
-     * The constructor
-     */
-    public EchoStatelessPlugin() {
-    }
+	/**
+	 * The constructor
+	 */
+	public EchoStatelessPlugin() {
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-     * )
-     */
-    public void start(BundleContext context) throws Exception {
-	super.start(context);
-	plugin = this;
-	Bundle bundle = context.getBundle();
-	OpenEjbApplication application = new OpenEjbApplication(bundle);
-	context.registerService(OpenEjbApplication.class.getName(),
-		application, null);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		Bundle bundle = context.getBundle();
+		OpenEjbApplication application = new OpenEjbApplication(bundle);
+		context.registerService(OpenEjbApplication.class.getName(), application, null);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-     * )
-     */
-    public void stop(BundleContext context) throws Exception {
-	plugin = null;
-	super.stop(context);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
 
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static EchoStatelessPlugin getDefault() {
-	return plugin;
-    }
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static EchoStatelessPlugin getDefault() {
+		return plugin;
+	}
 
 }
