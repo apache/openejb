d31f97d7e04ab58ac9927f9c5dffdc1878a158f6
David Blevins <dblevins@apache.org>
2011-05-28 23:05:18 +0000

    Stub project for some neat tooling ideas that came up on IRC
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk@1128794 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/tools/src/main/java/org/apache/openejb/tools/examples/GenerateIndex.java
@@ -0,0 +1,87 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.openejb.tools.examples;
+
+/**
+ * Most the examples do not have any documentation.
+ *
+ * There are some wiki pages for some of the examples, but these
+ * are hard to create and maintain.  The examples change frequently
+ * enough that we really should have documentation that goes with
+ * each version of the examples.
+ *
+ * If we put a README.md file in each example and use Markdown
+ * which is a really simple text format that has many tools
+ * capable of generating html, we could probably generate a
+ * web page for each example.  Then we could generate a index
+ * for all the examples.
+ *
+ * We could then take this all and upload it to the website
+ *
+ * Something kind of like this:
+ *    http://people.apache.org/~dblevins/simple-stateless.html
+ *
+ * But nicer looking, with breadcrumbs and links for navigating
+ * around to other examples
+ *
+ *
+ * IDEAS FOR AFTER SOMETHING IS WORKING
+ *
+ * Perhaps at some point some xref style processing of the source
+ * and links to the source
+ *
+ * Perhaps at some point use ASM to see what annotations and API
+ * classes were used and make an index that lists examples by
+ * which APIs are used
+ *
+ * Perhaps at some point use Swizzle stream to do a sort of SNIPPET
+ * thing like the Confluence plugin, so we wouldn't have to copy
+ * source into the example
+ *
+ * @version $Rev$ $Date$
+ */
+public class GenerateIndex {
+
+    // A couple possible markdown processors in Java
+    //   http://code.google.com/p/markdownj/wiki/Maven
+    //   http://code.google.com/p/doxia-module-markdown/wiki/Usage
+
+
+    // Syntax highlighting can be done with this:
+    //   http://code.google.com/p/google-code-prettify
+
+    /**
+     * Can be run in an IDE or via Maven like so:
+     *
+     * mvn clean install exec:java -Dexec.mainClass=org.apache.openejb.tools.examples.GenerateIndex
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // crack open the examples zip file
+
+        // create a directory for each example
+
+        // use the README.md markdown file to generate an index.html page
+
+        // If there is no README.md we should just generate a basic page
+        // maybe something that includes the FooTest.java code and shows
+        // shows that with links to other classes in the example
+
+        // create an index for all example directories
+    }
+}
---
+++ sandbox/tools/src/main/java/org/apache/openejb/tools/twitter/Retweet.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.openejb.tools.twitter;
+
+/**
+ *
+ * We should monitor this feed http://twitter.com/#!/OpenEJB/contributors
+ * and retweet anything that mentions OpenEJB
+ *
+ * So if anyone in the contributors list tweeted about OpenEJB, the OpenEJB twitter account would retweet it
+ *
+ * Two things will happen as a result:
+ *   -  The more activity on the OpenEJB twitter account the more followers it will get
+ *   -  The more @joe and other contributors are seen on the account, the more followers they will get
+ *
+ * The OpenEJB twitter account has more followers than most everyone else so getting it
+ * to retweet is a good way to expose people to all our wonderful contributors
+ * and get them some followers and help the project at the same time.
+ *
+ * The result is we as a community will have more ability overall to get the word out!
+ *
+ *
+ * @version $Rev$ $Date$
+ */
+public class Retweet {
+
+    public static void main(String[] args) {
+
+        // Grab the http://twitter.com/#!/OpenEJB/contributors feed via the Twitter API
+
+        // Scan for new tweets from the last hour
+
+        // Retweet any tweets that haven't been retweeted
+
+        // We could look at the OpenEJB twitter feed itself to determine if a tweet
+        // has already been retweeted
+
+    }
+}
