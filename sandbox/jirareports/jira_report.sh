a4d4ec749c8ffbf4a89fd530fee9ce4f8850a41a
David Blevins <dblevins@apache.org>
2011-07-17 20:30:19 +0000

    basic setup for jira reports
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk@1147710 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/jirareports/jira_report.sh
@@ -0,0 +1,19 @@
+#!/usr/local/bin/bash
+
+java=/usr/local/bin/java
+swizzle=$HOME/swizzle-jirareport-1.2.2-SNAPSHOT-dep.jar
+
+template=${1?Specify a template name}
+id=${2?Specify the numeric id of the project}
+name=${3?Specify the name of the project}
+to=${4?Specify the address to where the report should be sent}
+url=${5:-jira}
+
+$java -jar $swizzle $1 \
+    -DserverUrl=https://issues.apache.org/$url/ \
+    -DprojectId=$id -DprojectName=$name \
+    -Demail=true \
+    -Dfrom=dblevins@apache.org \
+    -Dto=$to \
+    -Dusername=jirareport \
+    -Dpassword=ampad | /usr/sbin/sendmail -it
