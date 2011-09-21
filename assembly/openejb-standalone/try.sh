67fb6bef09648cec7155508315984158aedb26e5
Haihong Xu <xuhaihong@apache.org>
2011-09-21 01:24:51 +0000

    Prepare for 3.0.4 release, update the version number in sh/build.xml/*.html file
    git-svn-id: https://svn.apache.org/repos/asf/openejb/branches/openejb-3.0.x@1173455 13f79535-47bb-0310-9956-ffa450edef68

--- assembly/openejb-standalone/try.sh
+++ assembly/openejb-standalone/try.sh
@@ -17,12 +17,12 @@
 #
 cd "$(dirname "$0")"
 
-JAR=$PWD/../../itests/openejb-itests-app/target/openejb-itests-app-3.0.4-SNAPSHOT.ear
+JAR=$PWD/../../itests/openejb-itests-app/target/openejb-itests-app-3.0.4.ear
 
 cd target/
 
-tar xzvf openejb-3.0.4-SNAPSHOT-bin.tar.gz
-cd openejb-3.0.4-SNAPSHOT
+tar xzvf openejb-3.0.4-bin.tar.gz
+cd openejb-3.0.4
 cp $JAR apps/
 ./bin/openejb start &
 sleep 1
--- assembly/openejb-tomcat/try.sh
+++ assembly/openejb-tomcat/try.sh
@@ -17,7 +17,7 @@
 #
 cd target/
 
-tar xzvf openejb-3.0.4-SNAPSHOT-bin.tar.gz
-cd openejb-3.0.4-SNAPSHOT
+tar xzvf openejb-3.0.4-bin.tar.gz
+cd openejb-3.0.4
 ./bin/openejb start
 
