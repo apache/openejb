e5f3c14dafea4ccdaf6d776de3fe7c34b6a44c6b
Jonathan Gallimore <jgallimore@apache.org>
2011-09-28 22:08:01 +0000

    OPENEJB-1687 use common classes
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk@1177089 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/arquillian-tomee/arquillian-tomee-remote-with-zip/src/main/java/org/apache/openejb/arquillian/remote/ContainersImplTomEE.java
+++ sandbox/arquillian-tomee/arquillian-tomee-remote-with-zip/src/main/java/org/apache/openejb/arquillian/remote/ContainersImplTomEE.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.openejb.arquillian;
+package org.apache.openejb.arquillian.remote;
 
 import java.io.Closeable;
 import java.io.File;
--- sandbox/arquillian-tomee/arquillian-tomee-remote-with-zip/src/main/java/org/apache/openejb/arquillian/remote/RemoteTomEEContainer.java
+++ sandbox/arquillian-tomee/arquillian-tomee-remote-with-zip/src/main/java/org/apache/openejb/arquillian/remote/RemoteTomEEContainer.java
@@ -14,39 +14,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.openejb.arquillian;
+package org.apache.openejb.arquillian.remote;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collection;
 
-import org.jboss.arquillian.spi.client.container.DeployableContainer;
-import org.jboss.arquillian.spi.client.container.DeploymentException;
+import org.apache.openejb.arquillian.common.SimpleMavenBuilderImpl;
+import org.apache.openejb.arquillian.common.TomEEContainer;
+import org.apache.openejb.config.RemoteServer;
 import org.jboss.arquillian.spi.client.container.LifecycleException;
-import org.jboss.arquillian.spi.client.protocol.ProtocolDescription;
-import org.jboss.arquillian.spi.client.protocol.metadata.HTTPContext;
-import org.jboss.arquillian.spi.client.protocol.metadata.ProtocolMetaData;
-import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.GenericArchive;
 import org.jboss.shrinkwrap.api.exporter.ExplodedExporter;
-import org.jboss.shrinkwrap.api.exporter.ZipExporter;
-import org.jboss.shrinkwrap.descriptor.api.Descriptor;
 import org.jboss.shrinkwrap.resolver.impl.maven.filter.StrictFilter;
 
-public class TomEEContainer implements DeployableContainer<TomEEConfiguration> {
+public class RemoteTomEEContainer extends TomEEContainer {
 	private static final String OPENEJB_VERSION = "4.0.0-beta-1-SNAPSHOT";
 
-    private TomEEConfiguration configuration;
-	private ContainersImplTomEE container;
-
-    public Class<TomEEConfiguration> getConfigurationClass() {
-        return TomEEConfiguration.class;
-    }
-
-    public void setup(TomEEConfiguration configuration) {
-        this.configuration = configuration;
-    }
+	private RemoteServer container;
 
     public void start() throws LifecycleException {
     	File catalinaDirectory = new File(configuration.getDir());
@@ -87,61 +71,19 @@ public class TomEEContainer implements DeployableContainer<TomEEConfiguration> {
     	}
     	
     	System.setProperty("tomee.http.port", String.valueOf(configuration.getHttpPort()));
-    	System.setProperty("tomee.shutdown.port", String.valueOf(configuration.getShutdownPort()));
+    	System.setProperty("tomee.shutdown.port", String.valueOf(configuration.getStopPort()));
     	System.setProperty("java.naming.provider.url","http://localhost:" + configuration.getHttpPort() + "/openejb/ejb");
     	System.setProperty("connect.tries","90");
     	System.setProperty("server.http.port", String.valueOf(configuration.getHttpPort()));
-    	System.setProperty("server.shutdown.port", String.valueOf(configuration.getShutdownPort()));
+    	System.setProperty("server.shutdown.port", String.valueOf(configuration.getStopPort()));
     	System.setProperty("java.opts", "-Xmx512m -Xms256m -XX:PermSize=64m -XX:MaxPermSize=256m -XX:ReservedCodeCacheSize=64m");
     	System.setProperty("openejb.home", openejbHome.getAbsolutePath());
-    	
-    	container = new ContainersImplTomEE();
-    	try {
-			container.setup();
-		} catch (IOException e) {
-			throw new LifecycleException("Unable to start TomEE", e);
-		}
+
+    	container = new RemoteServer();
+		container.start();
     }
 
     public void stop() throws LifecycleException {
-    	try {
-			container.cleanup();
-		} catch (IOException e) {
-			throw new LifecycleException("Unable to stop TomEE", e);
-		}
-    }
-
-    public ProtocolDescription getDefaultProtocol() {
-        return new ProtocolDescription("Servlet 3.0");
-    }
-    
-    public ProtocolMetaData deploy(Archive<?> archive) throws DeploymentException {
-    	try {
-        	InputStream is = archive.as(ZipExporter.class).exportAsInputStream();
-        	String name = archive.getName();
-        	container.deploy(is, name);
-        	
-        	HTTPContext httpContext = new HTTPContext("0.0.0.0", configuration.getHttpPort());
-            return new ProtocolMetaData().addContext(httpContext);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new DeploymentException("Unable to deploy", e);
-        }
-    }
-
-    public void undeploy(Archive<?> archive) throws DeploymentException {
-    	try {
-			container.undeploy(archive.getName());
-		} catch (IOException e) {
-			throw new DeploymentException("Unable to undeploy", e);
-		}
-    }
-
-    public void deploy(Descriptor descriptor) throws DeploymentException {
-        throw new UnsupportedOperationException("Not implemented");
-    }
-
-    public void undeploy(Descriptor descriptor) throws DeploymentException {
-        throw new UnsupportedOperationException("Not implemented");
+    	container.stop();
     }
 }
