30f4197636238a94c1958ed91f3fe6a03310523e
Jonathan Gallimore <jgallimore@apache.org>
2011-11-09 23:42:15 +0000

    TOMEE-37 more work on the WebappDeployer. Moved to its own module, and extended to work with ejb-jars and ears
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk@1200043 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/webdeployer/src/main/java/org/apache/tomee/catalina/deployer/WebappDeployer.java
+++ sandbox/webdeployer/src/main/java/org/apache/tomee/catalina/deployer/WebappDeployer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.tomee.catalina.deploy;
+package org.apache.tomee.catalina.deployer;
 
 import static javax.ejb.TransactionManagementType.BEAN;
 
@@ -29,6 +29,11 @@ import javax.ejb.TransactionManagement;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
+import org.apache.catalina.Container;
+import org.apache.catalina.Engine;
+import org.apache.catalina.Service;
+import org.apache.catalina.core.StandardHost;
+import org.apache.catalina.core.StandardServer;
 import org.apache.catalina.util.ContextName;
 import org.apache.openejb.NoSuchApplicationException;
 import org.apache.openejb.OpenEJBException;
@@ -36,9 +41,12 @@ import org.apache.openejb.UndeployException;
 import org.apache.openejb.assembler.Deployer;
 import org.apache.openejb.assembler.classic.AppInfo;
 import org.apache.openejb.assembler.classic.Assembler;
+import org.apache.openejb.assembler.classic.WebAppBuilder;
 import org.apache.openejb.loader.FileUtils;
 import org.apache.openejb.loader.SystemInstance;
 import org.apache.tomcat.util.modeler.Registry;
+import org.apache.tomee.catalina.TomcatWebAppBuilder;
+import org.apache.tomee.loader.TomcatHelper;
 
 @Stateless(name = "openejb/WebappDeployer")
 @Remote(Deployer.class)
@@ -47,11 +55,13 @@ public class WebappDeployer implements Deployer {
 
 	private static final String WEBAPPS = "webapps";
 	private static final String OPENEJB_HOME = "openejb.home";
+	private final TomcatWebAppBuilder webappBuilder;
 	private final Assembler assembler;
-	private MBeanServer mBeanServer;
+	private final MBeanServer mBeanServer;
 
 	public WebappDeployer() {
 		assembler = (Assembler) SystemInstance.get().getComponent(org.apache.openejb.spi.Assembler.class);
+		webappBuilder = (TomcatWebAppBuilder) SystemInstance.get().getComponent(WebAppBuilder.class);
 		mBeanServer = Registry.getRegistry(null, null).getMBeanServer();
 	}
 
@@ -74,57 +84,133 @@ public class WebappDeployer implements Deployer {
 	public AppInfo deploy(String location, Properties properties) throws OpenEJBException {
 		try {
 			if (location == null && properties == null) {
-			    throw new NullPointerException("location and properties are null");
+				throw new NullPointerException("location and properties are null");
 			}
 			if (location == null) {
-			    location = properties.getProperty(FILENAME);
+				location = properties.getProperty(FILENAME);
 			}
 			if (properties == null) {
-			    properties = new Properties();
+				properties = new Properties();
 			}
-			
-			// copy to the webapps folder
+
 			File source = new File(location);
 			File destination = new File(System.getProperty(OPENEJB_HOME) + File.separator + WEBAPPS + File.separator + source.getName());
 			FileUtils.copyFile(destination, source);
-			
-			// call check method
+
 			String destinationWithoutExtension = destination.getAbsolutePath();
 			String destinationFilenameWithoutExtension = destination.getName();
-			
+
 			if (destination.getName().contains(".")) {
 				destinationWithoutExtension = destinationWithoutExtension.substring(0, destinationWithoutExtension.lastIndexOf('.'));
 				destinationFilenameWithoutExtension = destinationFilenameWithoutExtension.substring(0, destinationFilenameWithoutExtension.lastIndexOf('.'));
 			}
-			
-			ContextName cn = new ContextName(destinationFilenameWithoutExtension);
-			
-			String name = "Catalina:type=Deployer,host=localhost";
-			ObjectName oname = new ObjectName(name);
-			
-			String[] params = { cn.getName() };
-	        String[] signature = { "java.lang.String" };
-	        mBeanServer.invoke(oname, "check", params, signature);
-			
-	        Collection<AppInfo> deployedApps = getDeployedApps();
 
-	        // find the correct appinfo
-			Iterator<AppInfo> iterator = deployedApps.iterator();
-			while (iterator.hasNext()) {
-				AppInfo appInfo = (AppInfo) iterator.next();
-				if (appInfo.path.equals(destinationWithoutExtension)) {
-					return appInfo;
-				}
+			if (destination.getName().toLowerCase().endsWith(".war")) {
+				checkWebapp(destinationFilenameWithoutExtension);
+			} else {
+				check();
 			}
-			
-			return null;
+
+			return findAppInfo(new String[] { destination.getAbsolutePath(), destinationWithoutExtension });
 		} catch (Exception e) {
 			throw new OpenEJBException(e);
 		}
-    }
+	}
 
-	public void undeploy(String moduleId) throws UndeployException,	NoSuchApplicationException {
-		assembler.destroyApplication(moduleId);
+	private AppInfo findAppInfo(String... paths) {
+		Collection<AppInfo> deployedApps = getDeployedApps();
+
+		Iterator<AppInfo> iterator = deployedApps.iterator();
+		while (iterator.hasNext()) {
+			AppInfo appInfo = (AppInfo) iterator.next();
+			for (String path : paths) {
+				if (appInfo.path.equals(path)) {
+					return appInfo;
+				}
+			}
+		}
+		
+		return null;
+	}
+
+	private void check() {
+		StandardServer server = TomcatHelper.getServer();
+		for (Service service : server.findServices()) {
+			if (service.getContainer() instanceof Engine) {
+				Engine engine = (Engine) service.getContainer();
+				for (Container engineChild : engine.findChildren()) {
+					if (engineChild instanceof StandardHost) {
+						StandardHost host = (StandardHost) engineChild;
+						webappBuilder.checkHost(host);
+					}
+				}
+			}
+		}
+	}
+
+	private void checkWebapp(String webappName) {
+		try {
+			ContextName cn = new ContextName(webappName);
+
+			String name = "Catalina:type=Deployer,host=localhost";
+			ObjectName oname = new ObjectName(name);
+
+			String[] params = { cn.getName() };
+			String[] signature = { "java.lang.String" };
+			mBeanServer.invoke(oname, "check", params, signature);
+		} catch (Exception e) {
+		}
+	}
+
+	public void undeploy(String moduleId) throws UndeployException, NoSuchApplicationException {
+		try {
+			AppInfo appInfo = findAppInfo(moduleId);
+			if (appInfo !=  null) {
+				webappBuilder.undeployWebApps(appInfo);
+			}
+			
+			assembler.destroyApplication(moduleId);
+
+			File moduleFile = new File(moduleId);
+			
+			if (moduleFile.getName().contains(".")) {
+				// delete matching directory
+				File dir = new File(moduleFile.getAbsolutePath().substring(0, moduleFile.getAbsolutePath().lastIndexOf('.')));
+				if (dir.exists() && dir.isDirectory()) {
+					delete(dir);
+				}
+			} else {
+				delete(new File(moduleFile + ".war"));
+				delete(new File(moduleFile + ".ear"));
+				delete(new File(moduleFile + ".rar"));
+			}
+			
+			delete(moduleFile);
+
+		} catch (Exception e) {
+			throw new UndeployException(e);
+		}
+	}
+
+	private void delete(File f) {
+		if (f == null || (!f.exists())) {
+			return;
+		}
+		
+		if (f.isFile()) {
+			f.delete();
+			return;
+		}
+		
+		if (f.isDirectory()) {
+			File[] listFiles = f.listFiles();
+			for (File file : listFiles) {
+				if (file.getName().equals(".") || file.getName().equals("..")) continue;
+				delete(file);
+			}
+			
+			f.delete();
+		}
 	}
 
 }
