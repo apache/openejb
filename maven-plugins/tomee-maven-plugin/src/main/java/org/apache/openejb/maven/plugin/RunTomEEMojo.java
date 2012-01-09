ddcdc79657f784045c708ba439014bad675c80e7
Romain Manni-Bucau <rmannibucau@apache.org>
2012-01-09 23:47:20 +0000

    making tomee:run working and logging
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk@1229420 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/tomee-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/RunTomEEMojo.java
+++ maven-plugins/tomee-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/RunTomEEMojo.java
@@ -18,21 +18,24 @@ package org.apache.openejb.maven.plugin;/*
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.resolver.ArtifactCollector;
+import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
+import org.apache.maven.artifact.repository.DefaultArtifactRepository;
+import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.archiver.manager.ArchiverManager;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
@@ -41,6 +44,9 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import static org.apache.maven.artifact.Artifact.SCOPE_COMPILE;
+import static org.apache.maven.artifact.repository.ArtifactRepositoryPolicy.CHECKSUM_POLICY_WARN;
+import static org.apache.maven.artifact.repository.ArtifactRepositoryPolicy.UPDATE_POLICY_DAILY;
+import static org.apache.maven.artifact.repository.ArtifactRepositoryPolicy.UPDATE_POLICY_NEVER;
 import static org.apache.maven.artifact.versioning.VersionRange.createFromVersion;
 import static org.codehaus.plexus.util.IOUtil.close;
 import static org.codehaus.plexus.util.IOUtil.copy;
@@ -51,13 +57,6 @@ import static org.codehaus.plexus.util.IOUtil.copy;
  */
 public class RunTomEEMojo extends AbstractMojo {
     /**
-     * @parameter expression="${project}"
-     * @required
-     * @readonly
-     */
-    protected MavenProject mavenProject;
-
-    /**
      * @component
      */
     protected ArtifactFactory factory;
@@ -66,13 +65,6 @@ public class RunTomEEMojo extends AbstractMojo {
      * @component
      */
     protected ArtifactResolver resolver;
-
-    /**
-     * @component role="org.apache.maven.artifact.resolver.ArtifactCollector"
-     * @required
-     * @readonly
-     */
-    protected ArtifactCollector artifactCollector;
 
     /**
      * @parameter expression="${localRepository}"
@@ -89,12 +81,7 @@ public class RunTomEEMojo extends AbstractMojo {
     protected List<ArtifactRepository> remoteRepos;
 
     /**
-     * @component
-     */
-    protected ArchiverManager archiverManager;
-
-    /**
-     * @parameter expression="${tomee-plugin.version}" default-value="1.0.0-beta-2"
+     * @parameter expression="${tomee-plugin.version}" default-value="1.0.0-beta-2-SNAPSHOT"
      */
     protected String tomeeVersion;
 
@@ -113,6 +100,11 @@ public class RunTomEEMojo extends AbstractMojo {
      * @readonly // while tar.gz is not managed
      */
     protected String tomeeType;
+
+    /**
+     * @parameter expression="${tomee-plugin.apache-repos}" default-value="snapshots"
+     */
+    protected String apacheRepos;
 
     /**
      * @parameter expression="${tomee-plugin.classifier}" default-value="webprofile"
@@ -140,10 +132,22 @@ public class RunTomEEMojo extends AbstractMojo {
     protected String tomeeCmd;
 
     /**
+     * @parameter expression="${tomee-plugin.args}"
+     */
+    protected String args;
+
+    /**
      * @parameter default-value="${project.build.directory}/apache-tomee"
      * @readonly
      */
     protected File catalinaBase;
+
+    /**
+     * relative to tomee.base.
+     *
+     * @parameter default-value="webapps"
+     */
+    protected String webappDir;
 
     /**
      * @parameter expression="${tomee-plugin.conf}" default-value="${basedir}/src/main/tomee/conf"
@@ -158,15 +162,15 @@ public class RunTomEEMojo extends AbstractMojo {
     protected File bin;
 
     /**
-     * @parameter default-value="${project.build.directory}/${project.build.finalName}"
-     * @readonly
-     */
-    protected File webappOutputDirectory;
-
-    /**
      * @parameter
      */
     protected Map<String, String> systemVariables = new HashMap<String, String>();
+
+    /**
+     * @parameter default-value="${project.build.directory}/${project.build.finalName}.${project.packaging}"
+     * @readonly
+     */
+    protected File warFile;
 
 
     @Override
@@ -174,11 +178,27 @@ public class RunTomEEMojo extends AbstractMojo {
         unzip(resolve(), catalinaBase);
         overrideConf(config);
         overrideConf(bin);
-        overridePorts();
+        overrideAddresses();
+        copyWar();
         run();
     }
 
-    private void overridePorts() {
+    private void copyWar() {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(warFile);
+            os = new FileOutputStream(new File(catalinaBase, webappDir + "/" + warFile.getName()));
+            copy(is, os);
+        } catch (Exception e) {
+            throw new TomEEException(e.getMessage(), e);
+        } finally {
+            close(is);
+            close(os);
+        }
+    }
+
+    private void overrideAddresses() {
         final File serverXml = new File(catalinaBase, "conf/server.xml");
         final String value = read(serverXml);
 
@@ -187,7 +207,9 @@ public class RunTomEEMojo extends AbstractMojo {
             writer = new FileWriter(serverXml);
             writer.write(value
                     .replace("8080", Integer.toString(tomeeHttpPort))
-                    .replace("8005", Integer.toString(tomeeShutdownPort)));
+                    .replace("8005", Integer.toString(tomeeShutdownPort))
+                    .replace("localhost", tomeeHost)
+                    .replace("webapps", webappDir));
             writer.close();
         } catch (IOException e) {
             throw new TomEEException(e.getMessage(), e);
@@ -240,29 +262,40 @@ public class RunTomEEMojo extends AbstractMojo {
     }
 
     private void run() {
+        final Process process;
         try {
-            baseProcessBuilder()
-                .command(java(),
-                        "-javaagent:" + new File(catalinaBase, "lib/openejb-javaagent.jar").getAbsolutePath(),
-                        "-cp", cp(), "org.apache.catalina.startup.Bootstrap", tomeeCmd)
-                .start().waitFor();
+            final ProcessBuilder builder = baseProcessBuilder();
+            builder.command(java(),
+                    "-javaagent:" + new File(catalinaBase, "lib/openejb-javaagent.jar").getAbsolutePath(),
+                    "-cp", cp());
+            if (args != null && args.length() > 0) {
+                builder.command().addAll(Arrays.asList(args.split(" ")));
+            }
+            builder.command().addAll(Arrays.asList("org.apache.catalina.startup.Bootstrap", tomeeCmd));
+            builder.redirectErrorStream(true);
+            process = builder.start();
         } catch (Exception e) {
             throw new TomEEException(e.getMessage(), e);
         }
 
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                try {
-                    baseProcessBuilder()
-                            .command(java(), "-cp", cp(), "org.apache.catalina.startup.Bootstrap", "stop")
-                            .start()
-                            .waitFor();
-                } catch (Exception e) {
-                    throw new TomEEException(e.getMessage(), e);
-                }
+        final InputStream is = process.getInputStream();
+        final BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line;
+        do {
+            try {
+                line = br.readLine();
+                getLog().info(line);
+            } catch (IOException e) {
+                line = null;
             }
-        });
+        } while (line != null);
+
+        Runtime.getRuntime().addShutdownHook(new ShutdownThread());
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            throw new TomEEException(e.getMessage(), e);
+        }
     }
 
     private static String java() {
@@ -294,11 +327,17 @@ public class RunTomEEMojo extends AbstractMojo {
             cpSep = ';';
         }
 
-        return new File(catalinaBase, "bin/bootstrap.jar").getAbsolutePath()
-                + cpSep + new File(catalinaBase, "bin/tomcat-juli.jar").getAbsolutePath();
+        return "bin/bootstrap.jar" + cpSep + "bin/tomcat-juli.jar";
     }
 
     private File resolve() {
+        if ("snapshots".equals(apacheRepos) || "true".equals(apacheRepos)) {
+            remoteRepos.add(new DefaultArtifactRepository("apache", "https://repository.apache.org/content/repositories/snapshots/",
+            new DefaultRepositoryLayout(),
+            new ArtifactRepositoryPolicy(true, UPDATE_POLICY_DAILY , CHECKSUM_POLICY_WARN),
+            new ArtifactRepositoryPolicy(false, UPDATE_POLICY_NEVER , CHECKSUM_POLICY_WARN)));
+        }
+
         try {
             final Artifact artifact = factory.createDependencyArtifact(tomeeGroupId, tomeeArtifactId, createFromVersion(tomeeVersion), tomeeType, tomeeClassifier, SCOPE_COMPILE);
             resolver.resolve(artifact, remoteRepos, local);
@@ -330,7 +369,10 @@ public class RunTomEEMojo extends AbstractMojo {
                 }
                 final File dest = new File(catalinaBase.getAbsolutePath(), name);
                 if (!dest.exists()) {
-                    dest.getParentFile().mkdirs();
+                    final File parent = dest.getParentFile();
+                    parent.mkdirs();
+                    parent.setWritable(true);
+                    parent.setReadable(true);
                 }
                 if (entry.isDirectory()) {
                     dest.mkdir();
@@ -342,6 +384,11 @@ public class RunTomEEMojo extends AbstractMojo {
                         // ignored
                     }
                     close(fos);
+
+                    dest.setReadable(true);
+                    if (dest.getName().endsWith(".sh")) {
+                        dest.setExecutable(true);
+                    }
                 }
             }
         } catch (Exception e) {
@@ -353,6 +400,19 @@ public class RunTomEEMojo extends AbstractMojo {
                 } catch (IOException e) {
                     // no-op
                 }
+            }
+        }
+    }
+
+    private class ShutdownThread extends Thread {
+        @Override public void run() {
+            try {
+                baseProcessBuilder()
+                        .command(java(), "-cp", cp(), "org.apache.catalina.startup.Bootstrap", "stop")
+                        .start()
+                        .waitFor();
+            } catch (Exception e) {
+                throw new TomEEException(e.getMessage(), e);
             }
         }
     }
