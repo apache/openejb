13c295d7ee2e1524912daf96321a660ef638b4bd
Romain Manni-Bucau <rmannibucau@apache.org>
2012-02-21 16:53:51 +0000

    externalizing profiles
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk@1291909 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/spi-helper-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/spi/SpiMojo.java
+++ maven-plugins/spi-helper-maven-plugin/src/main/java/org/apache/openejb/maven/plugin/spi/SpiMojo.java
@@ -2,7 +2,6 @@ package org.apache.openejb.maven.plugin.spi;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DefaultArtifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.handler.DefaultArtifactHandler;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
@@ -28,6 +27,7 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.StringReader;
@@ -39,11 +39,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 /**
@@ -52,82 +53,13 @@ import java.util.Set;
  * @phase compile
  */
 public class SpiMojo extends AbstractMojo {
-    private static final Map<String, Profile> DEFAULT_PROFILES = new HashMap<String, Profile>();
-    private static final String DEFAULT_PROFILE = "jee6";
-
-    static {
-        final Profile jee6 = new Profile(
-                Arrays.asList( // annotations, TODO: possibly remove some redundant annotations
-                        "javax.annotation.ManagedBean",
-                        "javax.annotation.PostConstruct",
-                        "javax.annotation.PreDestroy",
-                        "javax.annotation.Resource",
-                        "javax.annotation.Resources",
-                        "javax.annotation.security.DenyAll",
-                        "javax.annotation.security.PermitAll",
-                        "javax.annotation.security.RolesAllowed",
-                        "javax.annotation.sql.DataSourceDefinition",
-                        "javax.ejb.AfterBegin",
-                        "javax.ejb.AfterCompletion",
-                        "javax.ejb.ApplicationException",
-                        "javax.ejb.Asynchronous",
-                        "javax.ejb.BeforeCompletion",
-                        "javax.ejb.ConcurrencyManagement",
-                        "javax.ejb.DependsOn",
-                        "javax.ejb.EJB",
-                        "javax.ejb.EJBs",
-                        "javax.ejb.Init",
-                        "javax.ejb.LocalBean",
-                        "javax.ejb.LocalHome",
-                        "javax.ejb.MessageDriven",
-                        "javax.ejb.PostActivate",
-                        "javax.ejb.PrePassivate",
-                        "javax.ejb.RemoteHome",
-                        "javax.ejb.Remove",
-                        "javax.ejb.Schedule",
-                        "javax.ejb.Schedules",
-                        "javax.ejb.Singleton",
-                        "javax.ejb.Stateful",
-                        "javax.ejb.Stateless",
-                        "javax.ejb.Timeout",
-                        "javax.enterprise.inject.Specializes",
-                        "javax.interceptor.AroundInvoke",
-                        "javax.interceptor.AroundTimeout",
-                        "javax.interceptor.ExcludeClassInterceptors",
-                        "javax.interceptor.ExcludeDefaultInterceptors",
-                        "javax.interceptor.Interceptors",
-                        "javax.jws.WebService",
-                        "javax.persistence.PersistenceContext",
-                        "javax.persistence.PersistenceContexts",
-                        "javax.persistence.PersistenceUnit",
-                        "javax.persistence.PersistenceUnits",
-                        "javax.ws.rs.Path",
-                        "javax.xml.ws.WebServiceProvider",
-                        "javax.xml.ws.WebServiceRef",
-                        "javax.xml.ws.WebServiceRefs"
-                ),
-                Arrays.asList( // subclasses
-                        "javax.ws.rs.core.Application"
-                ),
-                new ArrayList<String>( // implementations
-                        // no implementations
-                )
-        );
-        DEFAULT_PROFILES.put(DEFAULT_PROFILE, jee6);
-    }
+    private static final String PROFILE_PATH = "org/apache/xbean/profile.properties";
 
     /**
      * @parameter default-value="${project.build.outputDirectory}"
      * @required
      */
     private File module;
-
-    /**
-     * @parameter default-value="${project.build.outputDirectory}"
-     *
-     * for webapps "${project.build.directory}/${project.build.finalName}" is better
-     */
-    private File outputFolder;
 
     /**
      * @parameter default-value="${project}"
@@ -157,7 +89,9 @@ public class SpiMojo extends AbstractMojo {
     private List<String> profiles;
 
     /**
-     * @parameter expression="${spi.output}" default-value="META-INF/org/apache/xbean/scan.xml"
+     * @parameter expression="${spi.output}" default-value="${project.build.outputDirectory}/META-INF/org/apache/xbean/scan.xml"
+     *
+     * for webapp: ${project.build.directory}/${project.build.finalName}/WEB-INF/org/apache/xbean/scan.xml
      */
     private String outputFilename;
 
@@ -194,25 +128,22 @@ public class SpiMojo extends AbstractMojo {
     /**
      * @component
      */
-    protected ArtifactFactory factory;
-
-    /**
-     * @component
-     */
     protected ArtifactResolver resolver;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        final Map<String, Profile> loadedProfiles = loadProfiles();
+
         //
         // create profiles to use
         //
         final List<Profile> profileToUse = new ArrayList<Profile>();
         if (profiles != null) {
             for (String profile : profiles) {
-                if (DEFAULT_PROFILES.containsKey(profile)) {
-                    profileToUse.add(DEFAULT_PROFILES.get(profile));
+                if (loadedProfiles.containsKey(profile)) {
+                    profileToUse.add(loadedProfiles.get(profile));
                 } else {
-                    getLog().info("can't find profile " + profile + ", available ones are " + DEFAULT_PROFILES.keySet());
+                    getLog().info("can't find profile " + profile + ", available ones are " + loadedProfiles.keySet());
                 }
             }
         }
@@ -220,8 +151,15 @@ public class SpiMojo extends AbstractMojo {
             profileToUse.add(new Profile(annotations, subclasses, implementations));
         }
 
+        if (profileToUse.isEmpty() && loadedProfiles.size() > 0) {
+            final Map.Entry<String, Profile> profile = loadedProfiles.entrySet().iterator().next();
+            getLog().info("using profile " + profile.getKey());
+            profileToUse.add(profile.getValue());
+        }
+
         if (profileToUse.isEmpty()) {
-            profileToUse.add(DEFAULT_PROFILES.get(DEFAULT_PROFILE));
+            getLog().warn("no profile or configuration, nothing will be done");
+            return;
         }
 
         //
@@ -233,7 +171,6 @@ public class SpiMojo extends AbstractMojo {
         final Archive archive;
         final URLClassLoader loader = createClassLoader(providedDependenciesClassLoader());
         if (war && useAggregatedArchiveIfWar) {
-            // no parent to avoid to not simply look lib/
             archive = new ClasspathArchive(loader, loader.getURLs());
             getLog().info("using an aggregated archive");
         } else {
@@ -323,7 +260,7 @@ public class SpiMojo extends AbstractMojo {
             // dump found classes
             //
 
-            final File output = path(outputFolder, outputFilename);
+            final File output = new File(outputFilename);
             if (!output.getParentFile().exists() && !output.getParentFile().mkdirs()) {
                 getLog().error("can't create " + output.getParent());
                 return;
@@ -352,12 +289,44 @@ public class SpiMojo extends AbstractMojo {
         }
     }
 
-    private File path(File outputFolder, String outputFilename) {
-        File output = new File(outputFilename);
-        if (!output.isAbsolute()) {
-            output = new File(outputFolder, outputFilename);
+    private Map<String, Profile> loadProfiles() {
+        final Map<String, Profile> profiles = new HashMap<String, Profile>();
+        try {
+            final Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources(PROFILE_PATH);
+            while (urls.hasMoreElements()) {
+                final URL url = urls.nextElement();
+                final Properties properties = new Properties();
+                properties.load(new BufferedInputStream(url.openStream()));
+
+                final String name = properties.getProperty("name");
+                if (name == null) {
+                    getLog().warn("ignoring " + url.toExternalForm() + " since it doesn't contain a name");
+                }
+                
+
+                final List<String> profileAnnotations = list(properties.getProperty("annotations"));
+                final List<String> profileSubclasses = list(properties.getProperty("subclasses"));
+                final List<String> profileImplementations = list(properties.getProperty("implementations"));
+
+                profiles.put(name, new Profile(profileAnnotations, profileSubclasses, profileImplementations));
+            }
+        } catch (Exception e) {
+            getLog().warn("can't look for profiles");
         }
-        return output;
+        return profiles;
+    }
+
+    private static List<String> list(final String commaSeparatedList) {
+        final List<String> list = new ArrayList<String>();
+        if (commaSeparatedList != null) {
+            for (String value : commaSeparatedList.split(",")) {
+                final String trimmed = value.trim();
+                if (!trimmed.isEmpty()) {
+                    list.add(trimmed);
+                }
+            }
+        }
+        return list;
     }
 
     private ClassLoader providedDependenciesClassLoader() {
