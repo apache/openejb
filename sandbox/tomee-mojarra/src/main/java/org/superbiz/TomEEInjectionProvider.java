67a2c484aab50539d22d21e8089c27e8721d2ca3
David Blevins <dblevins@apache.org>
2012-06-29 20:22:12 +0000

    Should work assuming:  1. Mojarra will pass in ServletContext as it seems it might  2. We add WebContext to the ServletContext attributes (we currently don't) TOMEE-259
    git-svn-id: https://svn.apache.org/repos/asf/openejb/trunk@1355551 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/tomee-mojarra/src/main/java/org/superbiz/TomEEInjectionProvider.java
+++ sandbox/tomee-mojarra/src/main/java/org/superbiz/TomEEInjectionProvider.java
@@ -2,35 +2,43 @@ package org.superbiz;
 
 import com.sun.faces.spi.DiscoverableInjectionProvider;
 import com.sun.faces.spi.InjectionProviderException;
+import org.apache.openejb.OpenEJBException;
+import org.apache.openejb.core.WebContext;
 import org.apache.webbeans.config.WebBeansContext;
 import org.apache.webbeans.inject.OWBInjector;
 
+import javax.servlet.ServletContext;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class TomEEInjectionProvider extends DiscoverableInjectionProvider {
-    private final Map<Object, OWBInjector> injectors = new ConcurrentHashMap<Object, OWBInjector>();
+
+    private final ServletContext servletContext;
+    private final WebContext webContext;
+
+    public TomEEInjectionProvider(ServletContext servletContext) {
+        this.servletContext = servletContext;
+        final Object attribute = servletContext.getAttribute(WebContext.class.getName());
+
+        if (attribute == null) throw new IllegalStateException("No WebContext found in ServletContext attributes");
+
+        if (!(attribute instanceof WebContext)) throw new IllegalStateException("WebContext entry in ServletContext attributes is not an instance of WebContext");
+
+        webContext = (WebContext) attribute;
+    }
 
     @Override
     public void inject(final Object managedBean) throws InjectionProviderException {
-        final OWBInjector injector = new OWBInjector(WebBeansContext.currentInstance());
-        injectors.put(managedBean, injector);
         try {
-            injectors.get(managedBean).inject(managedBean);
-        } catch (Exception e) {
+            webContext.inject(managedBean);
+        } catch (OpenEJBException e) {
             throw new InjectionProviderException(e);
         }
     }
 
     @Override
     public void invokePreDestroy(Object managedBean) throws InjectionProviderException {
-        if (injectors.containsKey(managedBean)) {
-            try {
-                injectors.remove(managedBean).destroy();
-            } catch (Exception e) {
-                throw new InjectionProviderException(e);
-            }
-        }
+        webContext.destroy(managedBean);
     }
 
     @Override
